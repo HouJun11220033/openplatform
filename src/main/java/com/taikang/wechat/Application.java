bf244b67dd9b46b61cfccdcb8a43aceb97ce1eec
zqs <444863055@qq.com>
2018-07-05 13:25:08 +0800

    version 1.0

--- src/main/java/com/taikang/wechat/Application.java
+++ src/main/java/com/taikang/wechat/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenplatformApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenplatformApplication.class, args);
+        SpringApplication.run(Application.class, args);
 
     }
 }

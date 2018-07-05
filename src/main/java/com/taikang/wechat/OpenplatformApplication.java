4ae76d82c50b54ccbd8945c674b5afab9613c3e2
lvgy <itw_lvgy@tkmail.com>
2018-07-05 11:07:01 +0800

    Initial commit

---
+++ src/main/java/com/taikang/wechat/OpenplatformApplication.java
@@ -0,0 +1,12 @@
+package com.taikang.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenplatformApplication.class, args);
+    }
+}
---
+++ src/test/java/com/taikang/wechat/OpenplatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.taikang.wechat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenplatformApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

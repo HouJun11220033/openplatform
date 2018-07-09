3203de3e500be6bebde7c19260e9f28749f27406
zhangqingsen <zhangqingsen@sinosoft.com.cn>
2018-07-09 12:24:27 +0800

    修改服务注册

--- src/main/java/com/taikang/wechat/Application.java
+++ src/main/java/com/taikang/wechat/Application.java
@@ -3,10 +3,14 @@ package com.taikang.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableDiscoveryClient
+@EnableFeignClients
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
